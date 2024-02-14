1ec53ba5de247e44eb3a8ce7285fd8bee5de99c3
bgsoto <115710071+bgsoto@users.noreply.github.com>
2024-02-14 17:41:42 -0500

    AI Test FrameWork
    Bryan_Soto_SoR:
    -AI Test Frame Work installed
    -Node abstract class script created
    -Composite Abstract class script created
    -Sequence Class created
    -Selector Class created

---
+++ Assets/Scripts/AI/Behaviors/Composite.cs
@@ -0,0 +1,13 @@
+using System.Linq;
+
+
+public abstract class Composite : Node
+{
+    protected int CurrentChildIndex = 0;
+    //constructor
+    protected Composite(string displayName, params Node[] childNodes)
+    {
+        Name = displayName;
+        ChildNodes.AddRange(childNodes.ToList());
+    }
+}
