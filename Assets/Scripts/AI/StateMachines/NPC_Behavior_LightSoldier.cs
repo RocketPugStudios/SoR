78b9e4d3087a6c7cfdf65a82f11879ba3cb95789
bgsoto <115710071+bgsoto@users.noreply.github.com>
2024-02-22 06:12:45 -0500

    misc
    Bryan_Sor:
    - save

--- Assets/Scripts/AI/StateMachines/NPC_Behavior_LightSoldier.cs
+++ Assets/Scripts/AI/StateMachines/NPC_Behavior_LightSoldier.cs
@@ -9,49 +9,63 @@ using UnityEngine.AI;
 public class NPC_Behavior_LightSoldier : MonoBehaviour
 {
     public NavMeshAgent agent {get;private set;}
-    public int patrolNodeElement;
+    public int patrolNodeIndex;
     [SerializeField] List<GameObject> PatrolNodes;
     public Queue<Transform> PatrolNodesQueue;
     public int numofPatrolNodes;
     public float distance;
+    public bool routeLoaded;
     private void Awake()
     {
         agent = GetComponent<NavMeshAgent>();
         getPatrolNodes();      
     }
-    private void Update()
+    private void FixedUpdate()
     {
-        agent.SetDestination(PatrolNodes[patrolNodeElement].transform.localPosition);
-        setNextDestination();
+      
+       
+        //agent.SetDestination(PatrolNodes[patrolNodeIndex].transform.localPosition);
+        // setNextDestination();
     }
     private void getPatrolNodes()
     {
+        // store waypoints into an array
         GameObject[] findNodes = GameObject.FindGameObjectsWithTag("PatrolNode");
+        
+        // loop each time a waypoint is found in an array then store the waypoint into a list.
         foreach (GameObject patrolNode in findNodes)
         {
             PatrolNodes.Add(patrolNode);
             numofPatrolNodes++;
+            Route(patrolNode.transform);
         }  
     }
+    /*
     private void setNextDestination()
     {
-        distance = Vector3.Distance(agent.transform.position, PatrolNodes[patrolNodeElement].transform.localPosition);
+        //distance = Vector3.Distance(agent.transform.position, PatrolNodes[patrolNodeIndex].transform.localPosition);
+        // When NPC reaches waypoint.
         if (distance <= 2f)
         {   
-            
-          patrolNodeElement++;
-          Debug.Log("NextNode");
-            
-          if (patrolNodeElement == numofPatrolNodes)
+          //go to next waypoint in list  
+          patrolNodeIndex++;
+          Debug.Log("go to next node");
+           //when NPC reaches all waypoints in list, go back in reverse order.
+          if (patrolNodeIndex == numofPatrolNodes)
           {
+                // reverse all nodes within list
                     PatrolNodes.Reverse();
-                    patrolNodeElement = 0;
+                    patrolNodeIndex = 0;
           }
         }     
     }
-
-    private void setNavigation(Transform position)
+    */
+    private void Route(Transform patrolNodes)
     {
-        PatrolNodesQueue.Enqueue(position); 
+        
+        PatrolNodesQueue.Enqueue(patrolNodes); 
+        
+
     }
+  
 }
--- Assets/Scripts/AI/StateMachines/NPC_behavior_StateMachine.cs
+++ Assets/Scripts/AI/StateMachines/NPC_behavior_StateMachine.cs
@@ -1,22 +1,87 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Net.NetworkInformation;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class NPC_behavior_StateMachine : MonoBehaviour
 {
-    public enum CharacterState
-    {
-        IdlePatrol,
-        ActivePatrol,
-        
-    }
 
-    // Define triggers if your state transitions depend on specific events
-    public enum Trigger
-    {
-        StartRunning,
-        StopRunning,
-        
-    }
 
+    public enum NPCState
+    {
+        Idle,
+        Patrol
+    }
+    public NPCState currentState = NPCState.Idle;
+
+        // Patrol behavior settings
+        public Transform[] patrolPoints;
+        private int currentPatrolPointIndex;
+        public NavMeshAgent agent { get; private set; }
+        public int patrolNodeIndex;
+        [SerializeField] List<GameObject> PatrolNodes;
+        public Queue<Transform> PatrolNodesQueue;
+        public int numofPatrolNodes;
+        public float distance;
+        public bool routeLoaded;
+
+    void Update()
+        {
+            switch (currentState)
+            {
+                case NPCState.Idle:
+                    IdleState();
+                    break;
+                case NPCState.Patrol:
+                    PatrolState();
+                    break;
+            }
+        }
+
+        void IdleState()
+        {
+           
+        }
+
+    void PatrolState()
+    {
+
+    
+        bool StartPatrol()
+        {
+            PlanRoute();
+            
+            return true;
+            
+        }
+
+        bool StopPatrolling()
+        {
+            if(PatrolNodesQueue.Count == 0)
+            {
+                currentState = NPCState.Idle;
+            }
+            return false;
+        }
+
+        void MoveTowards(Vector3 targetPosition)
+        {
+            agent.SetDestination(targetPosition);
+        }
+
+        void PlanRoute()
+        {
+            // store waypoints into an array
+            GameObject[] findNodes = GameObject.FindGameObjectsWithTag("PatrolNode");
+
+            // loop each time a waypoint is found in an array then store the waypoint into a list.
+            foreach (GameObject patrolNode in findNodes)
+            {
+                PatrolNodes.Add(patrolNode);
+                numofPatrolNodes++;
+               
+            }
+        }
+    }
 }
