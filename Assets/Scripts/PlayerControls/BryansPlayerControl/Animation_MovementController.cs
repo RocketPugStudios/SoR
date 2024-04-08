51bda33b7e9f3575e2aec313572a5d9e362c5b32
bgsoto <115710071+bgsoto@users.noreply.github.com>
2024-04-08 06:31:36 -0400

    controller update

--- Assets/Scripts/PlayerControls/BryansPlayerControl/Animation_MovementController.cs
+++ Assets/Scripts/PlayerControls/BryansPlayerControl/Animation_MovementController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEditor.Rendering.LookDev;
 using UnityEngine;
 using UnityEngine.InputSystem;
@@ -10,24 +11,65 @@ public class Animation_MovementController : MonoBehaviour
 
 
     Player_Xbox_Controls playerInput;
+    CharacterController characterController;
+
+    Vector2 currentMovementInput;
+    Vector3 currentMovement;
+    bool isMovementPressed;
 
 
     // Start is called before the first frame update
     void Awake()
     {
         playerInput = new Player_Xbox_Controls();
-        playerInput.CharacterControls.Move.started += Context => { };
+        characterController = GetComponent<CharacterController>();
 
-       
+        playerInput.CharacterControls.Move.started += Context =>
+        {
+
+            currentMovementInput = Context.ReadValue<Vector2>();
+            currentMovement.x = currentMovementInput.x;
+            currentMovement.z = currentMovementInput.y;
+            isMovementPressed = currentMovementInput.x != 0 || currentMovementInput.y != 0;
+
+
+
+
+        };
+
+        playerInput.CharacterControls.Move.canceled += Context =>
+        {
+
+            currentMovementInput = Context.ReadValue<Vector2>();
+            currentMovement.x = currentMovementInput.x;
+            currentMovement.z = currentMovementInput.y;
+            isMovementPressed = currentMovementInput.x != 0 || currentMovementInput.y != 0;
+
+
+        };
+
+        playerInput.CharacterControls.Move.performed += Context =>
+        {
+
+            currentMovementInput = Context.ReadValue<Vector2>();
+            currentMovement.x = currentMovementInput.x;
+            currentMovement.z = currentMovementInput.y;
+            isMovementPressed = currentMovementInput.x != 0 || currentMovementInput.y != 0;
+
+
+        };
     }
-
     // Update is called once per frame
     void Update()
     {
-        
+        characterController.Move(currentMovement * Time.deltaTime);
     }
     void OnEnable()
     {
-        
+        playerInput.CharacterControls.Enable();
+    }
+    void OnDisable()
+    {
+        playerInput.CharacterControls.Disable();
     }
 }
