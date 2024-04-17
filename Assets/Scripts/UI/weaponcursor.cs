af974644e2b78b6360612b3afb6071020e392404
bgsoto <115710071+bgsoto@users.noreply.github.com>
2024-04-17 10:25:18 -0400

    cross hair pack installed

---
+++ Assets/Scripts/UI/weaponcursor.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class weaponcursor : MonoBehaviour
+{
+    public Vector2 cursor;
+    [SerializeField]private Texture2D texture;
+
+
+     void Start()
+    {
+        cursor = new Vector2(texture.width /2 , texture.height / 2);
+        Cursor.SetCursor(texture,cursor,CursorMode.Auto);
+
+    }
+}
